4829886b058a3521e2dba65a7e5ad07ffba3fe42
Suvish Thoovamalayil <vishy1618@gmail.com>
2024-10-01 17:56:43 +0530

    Use image API for optimizing the image

--- components/hero-img.tsx
+++ components/hero-img.tsx
@@ -12,7 +12,7 @@ const HeroImage = ({ imageUrl, imageCaption, className }: HeroImageProps) => {
   return (
     <div className={cn(className, 'flex flex-col')}>
       <div className='relative flex-grow'>
-        <img className='object-cover' src={imageUrl} alt={imageCaption} />
+        <img className='object-cover' src={`${imageUrl}?width=440&height=440&quality=50`} alt={imageCaption} />
       </div>
       <p className='text-center text-sm py-2'>{imageCaption}</p>
     </div>
